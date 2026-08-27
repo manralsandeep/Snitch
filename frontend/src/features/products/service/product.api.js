d5e286ee3553260c3848dcbdce7da57b7735dc4a
manralsandeep <manralsandeep038@gmail.com>
2026-08-27 16:06:36 +0530

    url updated

--- frontend/src/features/products/service/product.api.js
+++ frontend/src/features/products/service/product.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const productApiInstance = axios.create({
-    baseURL: "/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
     withCredentials: true,
 })
 
